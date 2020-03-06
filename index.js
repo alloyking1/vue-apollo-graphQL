4a05c0b521dd71a80adf06407bae6d288d9c6c16
alloyking1 <alloyking1@gmail.com>
2020-03-06 16:21:39 -0500

    mutation fixed

--- index.js
+++ index.js
@@ -21,16 +21,15 @@ const typeDefs = gql`
     books: [Book]
   }
 
+  type Mutation {
+    createBook(title: String, author: String, description: String): Book
+  }
 
+  schema {
+    query: Query
+    mutation: Mutation
+  }
 `;
-
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
 
 const books = [
   {
@@ -44,11 +43,33 @@ const books = [
     description: 'Shit is getting real'
   },
   {
-      title: 'test book',
-      author: 'Moses west',
-      description: 'This is a demo graphQL project'
+    title: 'test book',
+    author: 'Moses west',
+    description: 'This is a demo graphQL project'
   }
 ];
+
+function save ({title, author, description}) {
+  let item = {title, author, description}
+  books.unshift(item)
+  return item
+}
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+
+  Mutation: {
+    async createBook (_, {title, author, description}) {
+      return await save({title, author, description})
+    }
+  }
+};
+
+
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
